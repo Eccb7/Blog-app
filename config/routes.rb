be21dd683628ded52fb952c9ef4b5447757b654d
Briton Otieno <ojwangbriton@gmail.com>
2023-10-25 20:29:55 +0300

    Add route paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define the root path route
+  root "users#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Users routes
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Posts routes
+  resources :posts, only: [:show]
 end
