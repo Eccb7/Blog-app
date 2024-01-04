54c6afe037c0e0d76413ded7afc4dbee35102df9
Briton Otieno <ojwangbriton@gmail.com>
2024-01-04 23:35:31 +0300

    Update users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    # return all users
+    @users = User.all
   end
 
   def show
-    # display a specific user
+    @user = User.find(params[:id])
+    @posts = User.recent_posts(params[:id])
   end
 end
